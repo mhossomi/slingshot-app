e28d760363754f579ee0bb577f127e56a99d7abd
Marcelo Hossomi <mhossomi@daitan.com>
2019-06-24 23:55:37 +0000

    Get port from env; script permissions.

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const slingshot = require('./src/slingshot')
 const bxml = require('./src/bxml')
 
-slingshot('Slingshot App', 8110, app => app
+slingshot('Slingshot App', process.env.PORT || 8110, app => app
     .use((req, res) => res.sendStatus(200)))
