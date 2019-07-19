4b0cd4e20791763197682cf0d56f7c78d385a622
mhossomi <mhossomi@daitan.com>
2019-07-19 17:18:30 -0300

    Added heroku URL discovery

--- src/heroku.js
+++ src/heroku.js
@@ -1,15 +1,14 @@
 const { execSync } = require('child_process')
 
 async function getPublicUrls() {
-    const remotes = execSync('git remote', { encoding: 'utf8' })
-    if (remotes.includes('heroku')) {
+    try {
+        const info = JSON.parse(execSync('heroku info --json'))
         console.log('Detected Heroku')
-        const info = JSON.parse(execSync('heroku apps:info --json'))
-        const name = info.app.name
         return [
-            { proto: 'http', public_url: `http://${name}.herokuapp.com` },
-            { proto: 'https', public_url: `https://${name}.herokuapp.com` }
+            { proto: 'https', public_url: info.app.web_url }
         ]
+    } catch (e) {
+        return undefined
     }
 }
 
