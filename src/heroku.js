8c787447a3c845654f41a4538f080df703b31abe
mhossomi <mhossomi@daitan.com>
2019-07-19 17:23:31 -0300

    Added Heroku detection

--- src/heroku.js
+++ src/heroku.js
@@ -2,6 +2,7 @@ const { execSync } = require('child_process')
 
 async function getPublicUrls() {
     try {
+        console.log(process.env)
         const info = JSON.parse(execSync('heroku info --json'))
         console.log('Detected Heroku')
         return [
