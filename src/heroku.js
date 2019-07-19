77ad5edcd4cc86a9e4e752ae0a8f870fe75581d8
mhossomi <mhossomi@daitan.com>
2019-07-19 17:35:10 -0300

    Fixed Heroku detection

--- src/heroku.js
+++ src/heroku.js
@@ -4,7 +4,8 @@ async function getPublicUrls() {
     if (process.env.HEROKU_APP_NAME) {
         console.log('Detected Heroku')
         return [
-            { proto: 'https', public_url: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` }
+            { proto: 'https', public_url: `https://${process.env.HEROKU_APP_NAME}.herokuapp.com` },
+            { proto: 'http', public_url: `http://${process.env.HEROKU_APP_NAME}.herokuapp.com` }
         ]
     }
 
