39cdadaf0c9fc3e37187931a4a028507d63ed998
mhossomi <mhossomi@daitan.com>
2019-09-09 13:54:14 -0300

    Add callback URL.

--- index.js
+++ index.js
@@ -4,6 +4,10 @@ const http = require('http')
 
 slingshot('Slingshot App', process.env.PORT || 8110, app => app
     .use('/complete', (req, res) => {
-        console.log('Recording complete!', JSON.stringify(req.body))
+        console.log('Recording complete!')
+        http.get(`${req.body.callUrl}/recordings/${req.body.recordingId}/mp3/mono`, res => {
+            console.log('Recording post-process triggered!', res.statusCode)
+        })
+        res.sendStatus(200)
     })
     .use((req, res) => res.sendStatus(200)))
