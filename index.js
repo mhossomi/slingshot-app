f96b1134a80aae30aef9f2f59281bb899ca2ac40
mhossomi <mhossomi@daitan.com>
2019-09-09 13:43:20 -0300

    Add callback URL.

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 const slingshot = require('./src/slingshot')
 const bxml = require('./src/bxml')
+const http = require('http')
 
 slingshot('Slingshot App', process.env.PORT || 8110, app => app
+    .use('/complete', (req, res) => {
+        console.log('Recording complete!', JSON.stringify(req.body))
+    })
     .use((req, res) => res.sendStatus(200)))
