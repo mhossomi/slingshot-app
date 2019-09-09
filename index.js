f6061f6deea7f8633acd1f1472d46364626b7459
mhossomi <mhossomi@daitan.com>
2019-09-09 14:02:29 -0300

    Add callback URL.

--- index.js
+++ index.js
@@ -5,9 +5,17 @@ const https = require('https')
 slingshot('Slingshot App', process.env.PORT || 8110, app => app
     .use('/complete', (req, res) => {
         console.log('Recording complete!')
-        https.get(`${req.body.callUrl}/recordings/${req.body.recordingId}/mp3/mono`, res => {
-            console.log('Recording post-process triggered!', res.statusCode)
-        })
-        res.sendStatus(200)
+
+        https.get(
+            `http://voice-media-service-mhossomi-worker.apps.lab1.ocp.bandwidth.com/accounts/9902319\
+            /calls/${req.body.callId}/recordings/${req.body.recordingId}/mp3/mono`,
+            res => console.log('Recording post-process triggered!', res.statusCode))
+            
+        res.send('<?xml version="1.0" ?>\
+        <Response> \
+            <SpeakSentence voice="susan"> \
+                Thanks! \
+            </SpeakSentence> \
+        </Response>')
     })
     .use((req, res) => res.sendStatus(200)))
