6e16183ca638190cfef34dcfd69922fa671a3acb
Marcelo Hossomi <mhossomi@daitan.com>
2019-11-08 10:31:26 -0200

    Reply with PlayAudio.

--- index.js
+++ index.js
@@ -1,23 +1,32 @@
 const slingshot = require('./src/slingshot')
 const bxml = require('./src/bxml')
+const config = require('./config.json')
 const fs = require('fs')
 const https = require('https')
+
+const authorization = "Basic " + Buffer.from(config.CALL_API_USERNAME + ":" + config.CALL_API_PASSWORD).toString("base64")
 
 slingshot('Slingshot App', process.env.PORT || 8110, app => app
     .use('/play-recording/:accountId/:callId/:recordingId', (req, res) => {
         const accountId = req.params.accountId
         const callId = req.params.callId
         const recordingId = req.params.recordingId
-        const mediaUrl = `https://censer:fJ2EENf68HV97CC@mhossomi.lab.voice.bandwidth.com/api/v2/accounts/${accountId}/calls/${callId}/recordings/${recordingId}/media`
+        const url = `${config.CALL_API_URL}/api/v2/accounts/${accountId}/calls/${callId}/recordings/${recordingId}/media`
 
-        console.log('Downloading:', mediaUrl)
+        console.log('Downloading:', url)
         const file = fs.createWriteStream('./audio/recording')
-        https.get(mediaUrl, (res) => {
+        https.get({ url, headers: { authorization } }, (res) => {
             res.pipe(file);
             file.on('finish', () => {
-		console.log('Done')
-		file.close()
-	    })
+                console.log('Done')
+                file.close()
+                res.send('<?xml version="1.0" ?>'
+                    + '<Response>'
+                    + '    <PlayAudio>'
+                    + '        ${env.HTTP_SELF}/audio/recording'
+                    + '   </PlayAudio>'
+                    + '</Response>')
+            })
         })
     })
     .use((req, res) => res.sendStatus(200)))
