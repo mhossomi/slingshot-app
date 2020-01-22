a7553cb1b623c5c89dd30b13fdadc9d1856fd380
mhossomi <mhossomi@daitan.com>
2020-01-22 10:15:36 -0300

    Added demo BXML.

--- index.js
+++ index.js
@@ -7,12 +7,13 @@ const https = require('https')
 const authorization = "Basic " + Buffer.from(config.CALL_API_USERNAME + ":" + config.CALL_API_PASSWORD).toString("base64")
 
 slingshot('Slingshot App', process.env.PORT || 8110, app => app
-    .use('/play-recording/:accountId/:callId/:recordingId', (req, res) => {
+    .use('/play-recording/accounts/:accountId/calls/:callId/recordings/:recordingId', (req, res) => {
         const accountId = req.params.accountId
         const callId = req.params.callId
         const recordingId = req.params.recordingId
         const url = `${config.CALL_API_URL}/api/v2/accounts/${accountId}/calls/${callId}/recordings/${recordingId}/media`
 
+        console.log(`Replied with <PlayAudio> for:\n${url}`)
         res.send('<?xml version="1.0" ?>'
             + '<Response>'
             + `    <PlayAudio username="${config.CALL_API_USERNAME}" password="${config.CALL_API_PASSWORD}">`
