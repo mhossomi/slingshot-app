73d26bb9dafaae1ecf98beb2663bc566619174a0
mhossomi <mhossomi@daitan.com>
2020-01-22 10:08:49 -0300

    Added demo BXML.

--- index.js
+++ index.js
@@ -13,22 +13,12 @@ slingshot('Slingshot App', process.env.PORT || 8110, app => app
         const recordingId = req.params.recordingId
         const url = `${config.CALL_API_URL}/api/v2/accounts/${accountId}/calls/${callId}/recordings/${recordingId}/media`
 
-        console.log('Downloading:', url)
-        const file = fs.createWriteStream('./audio/recording')
-        https.get(url, { headers: { authorization } }, (mediaRes) => {
-            mediaRes.pipe(file);
-            file.on('finish', () => {
-                console.log('Done')
-                file.close()
-                res.send('<?xml version="1.0" ?>'
-                    + '<Response>'
-                    + '    <PlayAudio>'
-                    + '        ${env.HTTP_SELF}/audio/recording'
-                    + '   </PlayAudio>'
-                    + '</Response>')
-            })
-        })
-	.on('error', e => console.log(e))
+        res.send('<?xml version="1.0" ?>'
+            + '<Response>'
+            + '    <PlayAudio>'
+            + `        ${url}`
+            + '   </PlayAudio>'
+            + '</Response>')
     })
     .use((req, res) => res.sendStatus(200)))
 
