54c953b0997633afeadc377047c50da6d40ec692
mhossomi <mhossomi@daitan.com>
2020-01-22 10:19:42 -0300

    Added demo BXML.

--- index.js
+++ index.js
@@ -1,22 +1,16 @@
 const slingshot = require('./src/slingshot')
-const bxml = require('./src/bxml')
-const config = require('./config.json')
-const fs = require('fs')
-const https = require('https')
-
-const authorization = "Basic " + Buffer.from(config.CALL_API_USERNAME + ":" + config.CALL_API_PASSWORD).toString("base64")
 
 slingshot('Slingshot App', process.env.PORT || 8110, app => app
     .use('/play-recording/accounts/:accountId/calls/:callId/recordings/:recordingId', (req, res) => {
         const accountId = req.params.accountId
         const callId = req.params.callId
         const recordingId = req.params.recordingId
-        const url = `${config.CALL_API_URL}/api/v2/accounts/${accountId}/calls/${callId}/recordings/${recordingId}/media`
+        const url = `${process.env.CALL_API_URL}/api/v2/accounts/${accountId}/calls/${callId}/recordings/${recordingId}/media`
 
         console.log(`Replied with <PlayAudio> for:\n${url}`)
         res.send('<?xml version="1.0" ?>'
             + '<Response>'
-            + `    <PlayAudio username="${config.CALL_API_USERNAME}" password="${config.CALL_API_PASSWORD}">`
+            + `    <PlayAudio username="${process.env.CALL_API_USERNAME}" password="${process.env.CALL_API_PASSWORD}">`
             + `        ${url}`
             + '   </PlayAudio>'
             + '</Response>')
